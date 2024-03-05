8d2643a19d38cca2c22877a6684ac308070e208e
hanjie1 <liu28238@gmail.com>
2024-03-05 16:41:02 -0500

    changes

--- ecal_cosmic_hls.cpp
+++ ecal_cosmic_hls.cpp
@@ -24,29 +24,101 @@ void ecal_cosmic_hls(
   static hit_t pre_fadc_hits[NCHAN];
 #pragma HLS ARRAY_PARTITION variable=pre_fadc_hits dim=1 type=complete
 
+  static hit_t pre_pre_fadc_hits[NCHAN];
+#pragma HLS ARRAY_PARTITION variable=pre_pre_fadc_hits dim=1 type=complete
+
+  smodule_t all_smo[3];
+
   ap_uint<2> ii=0;
   for(ii=0; ii<3; ii++){
      ap_uint<4> jj=0;
      hit_t smo_hits[9];
      hit_t pre_smo_hits[9];
+     hit_t pre_pre_smo_hits[9];
 
      for(jj=0; jj<9; jj++){
          ap_uint<5> ch=smodule_ch[ii][jj];
          smo_hits[jj] = fadc_hits.vxs_ch[ch];
          pre_smo_hits[jj] = pre_fadc_hits[ch];
+         pre_pre_smo_hits[jj] = pre_pre_fadc_hits[ch];
+
+         smodule_t tmp_smo;
+         tmp_smo = Find_Hits(jj, pre_pre_smo_hits, pre_smo_hits, smo_hits, hit_dt);
+         if(tmp_smo.nhits==0) continue;
+         
      } 
 
-     ap_uint<4> smo_t_min = Find_tmin(pre_smo_hits);
+   
     
   }
 
   ap_uint<5> ch=0;
 
   for(ch=0; ch<NCHAN; ch++){
-      fadc_hits_pre[ch] = fadc_hits.vxs_ch[ch];
+      pre_pre_fadc_hits[ch] = pre_fadc_hits[ch];
+  }
+
+  for(ch=0; ch<NCHAN; ch++){
+      pre_fadc_hits[ch] = fadc_hits.vxs_ch[ch];
   }
 
   return;
+}
+
+smodule_t Find_Hits(ap_uint<4> nblock, hit_t pre_fadc_hits[9], hit_t cur_fadc_hits[9], hit_t aft_fadc_hits[9], ap_uint<3> hit_dt){
+
+   smodule_t smo_final;
+   smo_final.n=0;
+   smo_final.t=0;
+   smo_final.nhits=0;
+
+   ap_uint<4> tmin = cur_fadc_hits[nblock].t;
+   ap_uint<1> tmp_nhits[9]={0};
+
+   ap_uint<1> bad=0;
+
+   ap_uint<4> jj=0;
+   for(jj=0; jj<9; jj++){
+       ap_uint<2> status=0;
+       if(cur_fadc_hits[jj].e>0)
+          status = hit_coin_t(t_min, cur_fadc_hits[jj].t, hit_dt); 
+       else if(pre_fadc_hits[jj].e>0)
+          status = hit_coin_t(t_min+8, pre_fadc_hits[jj].t, hit_dt); 
+       else if(aft_fadc_hits[jj].e>0)
+          status = hit_coin_t(t_min, aft_fadc_hits[jj].t+8, hit_dt); 
+
+       switch(status):{
+          case 1: tmp_nhits[jj]=1;break;
+          case 2: bad=1; break;
+       }
+   }
+
+   if(bad==0){
+      smo_final.n=nblock+1;
+      smo_final.t=tmin;
+      for(jj=0; jj<9; jj++)
+         smo_final.nhits +=  tmp_nhits[jj];
+   }
+   return smo_final;
+   
+}
+
+smodule_t Find_Hits(hit_t pre_fadc_hits){
+
+
+}
+
+ap_uint<2> hit_coin_t(ap_uint<4> t1, ap_uint<4> t2, ap_uint<3> dt) {
+  ap_uint<2> status=0;
+  
+  ap_uint<4> diff = (t1<t2) ? (t2-t1) : (t1-t2);
+
+  if(diff<=dt){
+     if(t1<=t2) status=1;
+     else status=2;   // t1 is not the leading time
+  }
+
+  return status;
 }
 
 ap_uint<1> sm_coin_t(ap_uint<4> t1, ap_uint<4> t2, ap_uint<3> dt) {
@@ -54,26 +126,3 @@ ap_uint<1> sm_coin_t(ap_uint<4> t1, ap_uint<4> t2, ap_uint<3> dt) {
   return (diff<=dt) ? 1 : 0;
 }
 
-ap_uint<1> hit_coin_t(ap_uint<4> t1, ap_uint<4> t2, ap_uint<3> dt) {
-  ap_uint<4> diff = (t1<=t2) ? (t2-t1) : 8;  //dt is only 3-bit, will never be 8
-  return (diff<=dt) ? 1 : 0;
-}
-
-
-ap_uint<3> Find_tmin(hit_t fadc_hits[9]){
-
-  ap_uint<4> ii=0;
-  ap_uint<4> t_min=8;
-  for(ii=0; ii<9; ii++){
-      if( fadc_hits[ii].e>0 && fadc_hits[ii].t<t_min){
-           t_min = fadc_hits[ii].t;
-      }
-  }
-
-  return t_min;
-}
-
-ap_uint<4> Find_multiplicity(hit_t fadc_hits[9], hit_t pre_fadc_hits, ap_uint<3> hit_dt){
-
-
-}
