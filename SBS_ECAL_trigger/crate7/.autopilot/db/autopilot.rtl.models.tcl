47c3cf5e1b2e108ed13bb39c63f056d1b01af72c
Hanjie Liu <liu28238@gmail.com>
2024-09-06 16:45:43 -0400

    Delete SBS_ECAL_trigger/crate7/.autopilot directory

--- SBS_ECAL_trigger/crate7/.autopilot/db/autopilot.rtl.models.tcl
+++
@@ -1,3 +0,0 @@
-set SynModuleInfo {
-  {SRCNAME ecal_cosmic_hls MODELNAME ecal_cosmic_hls RTLNAME c7ecal_cosmic_hls IS_TOP 1}
-}
--- SBS_ECAL_trigger/crate7/.autopilot/db/kernel_module_hierarchy.tcl
+++
@@ -1,3 +0,0 @@
-set ModuleHierarchy {[{
-"Name" : "ecal_cosmic_hls","ID" : "0","Type" : "pipeline"
-}]}
